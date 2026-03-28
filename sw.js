01c928f3e5c82981070a8062ed887a31281dcdca
MrSikandarYT <pakistanisikandar11@gmail.com>
2026-03-28 17:11:20 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -10,3 +10,10 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 10758219
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
